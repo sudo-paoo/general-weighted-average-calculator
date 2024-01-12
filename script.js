0922e97559a7ddff3667a0d7ee3beefecb6e05ac
Pao <kingpaolofranco60@gmail.com>
2024-01-12 11:00:55 +0800

    added remove button

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ function addSub() {
         <h3 class="subjectNumber">${++count}</h3>
         <input type="text" placeholder="Enter unit" class="unit__input">
         <input type="text" placeholder="Enter final grade" class="grade__input">
+		<button class="delete__button" onclick="removeSub(this)"><i class="ri-delete-bin-line"></i></button>
     </div>
   `;
   container.insertAdjacentHTML("beforeend", newContent);
@@ -34,3 +35,19 @@ function solve() {
     let gwaOutput = document.getElementById('gwa-output');
     gwaOutput.innerHTML = "GWA: " + (Math.round(gwa * 1000) / 1000);
 }
+
+function removeSub(button) {
+	let card = button.parentElement;
+
+	// Check if there's more than one subject before removing
+	if (count > 1) {
+		card.remove();
+		let subjectNumbers = document.querySelectorAll('.subjectNumber');
+		subjectNumbers.forEach((number, index) => {
+			number.textContent = index + 1;
+		});
+		count--;
+	} else {
+		alert("You cannot remove the last subject.");
+	}
+  }
